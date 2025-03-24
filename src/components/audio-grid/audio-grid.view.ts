7a1023ae9faa80033ca2baf3d520075cc60db613
william-hardin-software <118318782+guacardo@users.noreply.github.com>
2025-03-24 18:50:47 -0500

    switchin to PC

--- src/components/audio-grid/audio-grid.view.ts
+++ src/components/audio-grid/audio-grid.view.ts
@@ -22,88 +22,19 @@ export class AudioGridView extends LitElement {
     @property() private handleAddNode: AddNodeHandler;
     @property() handleUpdateNode: (node: GridAudioNode, properties: AudioNodeProperties) => void;
 
-    @state() private _grid: Map<number, LitElement[]>;
+    @state() private _audioGrid: LitElement[][];
 
     constructor() {
         super();
-        this._buildEmptyGrid();
+        this._fileNew();
     }
 
-    private _buildEmptyGrid() {
-        this._grid = new Map().set(0, [document.createElement("empty-node-view") as EmptyNodeView]);
-        this.requestUpdate();
-    }
-
-    private _renderGrid(): Map<number, LitElement[]> {
-        const renderGrid = new Map<number, LitElement[]>();
-        if (this.audioGraph.gridAudioNodes.length === 0) {
-            renderGrid.set(0, [document.createElement("empty-node-view") as EmptyNodeView]);
-        } else {
-            this.audioGraph.gridAudioNodes.map((node) => {
-                console.log(node);
-            });
-        }
-        return renderGrid;
-    }
-
-    private swapToNewNodeView(position: Position) {
-        const [rowIndex, colIndex] = position;
-        const row = this._grid.get(rowIndex);
-        if (row) {
-            const newNodeView = document.createElement("new-node-view") as NewNodeView;
-            const newRow = [...row];
-            newRow[colIndex] = newNodeView;
-            const newGrid = new Map(this._grid);
-            newGrid.set(rowIndex, newRow);
-            this._grid = newGrid;
-            this.requestUpdate();
-        }
+    private _fileNew() {
+        const foo = new Array();
+        foo[0][0] = document.createElement("empty-node-view") as EmptyNodeView;
     }
 
     render() {
-        console.log(this._renderGrid());
-        return html`
-            <div class="grid">
-                ${Array.from(this._grid.values()).map(
-                    (row, rowIndex) => html`
-                        <div class="row" key=${rowIndex}>
-                            ${row.map((node, colIndex) => {
-                                const position: Position = [rowIndex, colIndex];
-                                if (node instanceof EmptyNodeView) {
-                                    return html`
-                                        <empty-node-view
-                                            key=${colIndex}
-                                            .position=${position}
-                                            .handleSwapToNewNodeView=${this.swapToNewNodeView.bind(this)}
-                                        ></empty-node-view>
-                                    `;
-                                } else if (node instanceof NewNodeView) {
-                                    return html`<new-node-view .handleAddNode=${this.handleAddNode} .position=${position}></new-node-view>`;
-                                } else if (node instanceof GridGainNode) {
-                                    return html`<gain-node-view
-                                        .gainNode=${node as GridGainNode}
-                                        .audioGraph=${this.audioGraph}
-                                        .handleUpdateNode=${this.handleUpdateNode}
-                                    ></gain-node-view>`;
-                                } else if (node instanceof GridOscillatorNode) {
-                                    return html`<oscillator-node-view
-                                        .oscillatorNode=${node as GridOscillatorNode}
-                                        .audioGraph=${this.audioGraph}
-                                        .handleUpdateNode=${this.handleUpdateNode}
-                                    ></oscillator-node-view>`;
-                                } else if (node instanceof GridBiquadFilterNode) {
-                                    return html`<biquad-filter-node-view
-                                        .biquadFilterNode=${node}
-                                        .destination=${this.audioGraph?.context.destination}
-                                    ></biquad-filter-node-view>`;
-                                } else {
-                                    return html`<p>ERroR: nOT a n Audio Noooode</p>`;
-                                }
-                            })}
-                        </div>
-                    `
-                )}
-            </div>
-        `;
+        return html`<p>hello world</p>`;
     }
 }
